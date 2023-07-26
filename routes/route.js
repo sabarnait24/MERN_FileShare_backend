f1e25e2fed6196f4e37a44eddda35cb128080859
Sabarna Bhowmik <81832529+sabarnait24@users.noreply.github.com>
2023-07-26 21:11:17 +0530

    db deleted

--- routes/route.js
+++ routes/route.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const multer = require("multer");
 const router = express.Router();
-const { Fileschema } = require("../models/file");
 const upload = multer({ dest: "uploads/" });
 var cloudinary = require("cloudinary").v2;
-// import { unlink } from "node:fs/promises";
 const fs = require("fs");
 const path = require("path");
 
@@ -25,46 +23,19 @@ router.post("/upload", upload.single("Myfile"), async (req, res) => {
 
     const filedata = await cloudinary.uploader.upload(req.file.path, {});
 
-    const { originalname } = req.file;
-    const { secure_url, bytes, resource_type } = filedata;
-
-    const data = new Fileschema({
-      fileName: originalname,
-      urlLink: secure_url,
-      format: resource_type,
-      fileSize: bytes,
-    });
-    const savedata = await data.save();
-
-    res.json(savedata);
+    res.json(filedata);
   } catch (error) {
     res.send(error);
   }
   fs.readdir(directory, (err, files) => {
     if (err) throw err;
-  
+
     for (const file of files) {
       fs.unlink(path.join(directory, file), (err) => {
         if (err) throw err;
       });
     }
   });
- 
 });
 
-router.get("/upload/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const file = await Fileschema.findById(id);
-    if (!file) {
-      return res.status(500).send("File not found");
-    }
-    const { urlLink } = file;
-    // console.log(urlLink);/
-
-    res.status(200).json(urlLink);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
 module.exports = router;
