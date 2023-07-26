6d50822edd04ead746ea822a267cba0d80bf14a8
Sabarna Bhowmik <81832529+sabarnait24@users.noreply.github.com>
2023-07-26 21:37:50 +0530

    db deleted 1

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
 const express = require("express");
 const app = express();
-
 const port = process.env.PORT || 5000;
 const cors = require("cors");
-require('dotenv').config();
-const db = require("./db/db");
-const path = require('path');
-
+require("dotenv").config();
+const path = require("path");
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello from server");
-})
+});
 
 app.use("/api", require("./routes/route"));
 
 if (process.env.NODE_ENV === "production") {
   console.log("In production stage");
-  app.use(express.static(path.resolve(__dirname, '../', "client", "build")))
+  app.use(express.static(path.resolve(__dirname, "../", "client", "build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, '../', "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../", "client", "build", "index.html"));
   });
 }
-
 
 app.listen(port, () => {
   console.log(`connection is successful at ${port}`);
 });
-
